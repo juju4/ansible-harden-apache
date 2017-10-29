f7419c2dc25e4711c3d4c4a7e068a4ea0aa96e1d
juju4 <juju4@users.noreply.github.com>
2017-10-29 17:06:16 -0400

    formatting

--- test/integration/default/serverspec/webserver2_spec.rb
+++ test/integration/default/serverspec/webserver2_spec.rb
@@ -10,6 +10,7 @@ describe command('curl -kv https://localhost/'), :if => os[:family] == 'ubuntu' 
   its(:stderr) { should match /X-XSS-Protection: 1; mode=block/ }
   its(:stderr) { should match /default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'self';/ }
 end
+
 describe command('curl -kv https://localhost/'), :if => os[:family] == 'ubuntu' && os[:release] == '14.04' do
   its(:stdout) { should match /Apache2 Ubuntu Default Page: It works/ }
   its(:stderr) { should match /SSL connection using ECDHE-RSA-AES256-GCM-SHA384/ }
@@ -17,6 +18,7 @@ describe command('curl -kv https://localhost/'), :if => os[:family] == 'ubuntu' 
   its(:stderr) { should match /X-XSS-Protection: 1; mode=block/ }
   its(:stderr) { should match /default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'self';/ }
 end
+
 describe command('curl -kv https://localhost/'), :if => os[:family] == 'redhat' do
   its(:stdout) { should match /Apache HTTP Server Test Page powered by CentOS/ }
   its(:stderr) { should match /SSL connection using TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384/ }
@@ -24,6 +26,7 @@ describe command('curl -kv https://localhost/'), :if => os[:family] == 'redhat' 
   its(:stderr) { should match /X-XSS-Protection: 1; mode=block/ }
   its(:stderr) { should match /default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'self';/ }
 end
+
 describe command('curl -kv https://localhost/doesnotexist') do
   its(:stdout) { should match /404 Not Found/ }
 end
