4671f59d82a80c6c812a60c9093b80f51ded09e5
juju4 <juju4@users.noreply.github.com>
2017-11-18 11:34:37 -0500

    serverspec test: no server-status

--- test/integration/default/serverspec/webserver2_spec.rb
+++ test/integration/default/serverspec/webserver2_spec.rb
@@ -31,3 +31,6 @@ describe command('curl -kv https://localhost/doesnotexist') do
   its(:stdout) { should match /404 Not Found/ }
 end
 
+describe command('curl -kv https://localhost/server-status') do
+  its(:stdout) { should match /404 Not Found/ }
+end
