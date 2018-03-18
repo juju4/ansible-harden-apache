c4a4de81143de0a7b2bcb6ba01770bd3f4e7fb2d
juju4 <juju4@users.noreply.github.com>
2018-03-18 09:29:19 -0400

    fix serverspec tests issues

--- test/integration/default/serverspec/cspreport_spec.rb
+++ test/integration/default/serverspec/cspreport_spec.rb
@@ -6,5 +6,10 @@ set :backend, :exec
 describe command('curl -kv https://localhost/csp/report.php') do
   its(:stderr) { should match /HTTP\/1.1 204 No Content/ }
   its(:stderr) { should_not match /HTTP\/1.0 500 Internal Server Error/ }
+end
+describe command('curl -kv https://localhost/csp/report.php'), :if => os[:family] != 'ubuntu' || os[:release] != '18.04' do
   its(:stderr) { should match /SSL connection using TLS1.2/ }
+end
+describe command('curl -kv https://localhost/csp/report.php'), :if => os[:family] == 'ubuntu' && os[:release] == '18.04' do
+  its(:stderr) { should match /SSL connection using TLSv1.2/ }
 end
