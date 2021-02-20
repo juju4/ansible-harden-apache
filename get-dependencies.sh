b069de800522a66d5acf1266dbe2e399bf557d30
juju4 <juju4@users.noreply.github.com>
2021-02-20 16:41:43 -0500

    fix galaxy role name (2)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,6 @@ rolesdir=$(dirname $0)/..
 #[ ! -d $rolesdir/geerlingguy.nginx ] && git clone https://github.com/geerlingguy/ansible-role-nginx.git $rolesdir/geerlingguy.nginx
 [ ! -d $rolesdir/juju4.w3af ] && git clone https://github.com/juju4/ansible-w3af $rolesdir/juju4.w3af
 ## galaxy naming: kitchen fails to transfer symlink folder
-#[ ! -e $rolesdir/juju4.harden-apache ] && ln -s ansible-harden-apache $rolesdir/juju4.harden-apache
 [ ! -e $rolesdir/juju4.harden_apache ] && cp -R $rolesdir/ansible-harden-apache $rolesdir/juju4.harden_apache
 
 ## don't stop build on this script return code
