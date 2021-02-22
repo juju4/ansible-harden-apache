0a7cf145bb8c758d2e65dfd46efa6c0f2be5bc8b
juju4 <juju4@users.noreply.github.com>
2021-02-21 19:16:44 -0500

    test/default: add serverspec variable, update to current state (2)

--- test/integration/default/serverspec/cspreport_spec.rb
+++ test/integration/default/serverspec/cspreport_spec.rb
@@ -3,13 +3,19 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
+if (os[:family] == 'ubuntu')
+  set conn = 'SSL connection using TLSv1.3'
+else
+  set conn = 'SSL connection using TLSv1.2'
+end
+
 describe command('curl -kv https://localhost/csp/report.php') do
   its(:stderr) { should match /HTTP\/1.1 204 No Content/ }
   its(:stderr) { should_not match /HTTP\/1.0 500 Internal Server Error/ }
 end
 describe command('curl -kv https://localhost/csp/report.php'), :if => os[:family] != 'ubuntu' || os[:release] != '18.04' do
-  its(:stderr) { should match /SSL connection using TLS1.2/ }
+  its(:stderr) { should match /#{conn}/ }
 end
 describe command('curl -kv https://localhost/csp/report.php'), :if => os[:family] == 'ubuntu' && os[:release] == '18.04' do
-  its(:stderr) { should match /SSL connection using TLSv1.2/ }
+  its(:stderr) { should match /#{conn}/ }
 end
