8694e2037c0fdfb280d0122b472a290fee0e1bd8
juju4 <juju4@users.noreply.github.com>
2021-02-27 10:51:32 -0500

    test/default, serverspec: cspreport review and test

--- test/integration/default/serverspec/cspreport_spec.rb
+++ test/integration/default/serverspec/cspreport_spec.rb
@@ -6,20 +6,21 @@ set :backend, :exec
 set conn = 'SSL connection using TLSv1.3'
 if (os[:family] == 'ubuntu')
   set conn = 'SSL connection using TLSv1.3'
+elsif (os[:family] == 'centos' && os[:release] == '8')
+  set conn = 'SSL connection using TLSv1.3'
+elsif (os[:family] == 'redhat' && os[:release] == '7')
+  set conn = 'SSL connection using TLS_ECDHE'
+elsif (os[:family] == 'centos' && os[:release] == '7')
+  set conn = 'SSL connection using TLS_ECDHE'
 else
   set conn = 'SSL connection using TLSv1.2'
-end
-if (os[:family] == 'redhat' && os[:release] == '7')
-  set conn = 'SSL connection using TLS_ECDHE'
 end
 
 describe command('curl -kv https://localhost/csp/report.php') do
   its(:stderr) { should match /HTTP\/1.1 204 No Content/ }
   its(:stderr) { should_not match /HTTP\/1.0 500 Internal Server Error/ }
 end
-describe command('curl -kv https://localhost/csp/report.php'), :if => os[:family] != 'ubuntu' || os[:release] != '18.04' do
+describe command('curl -kv https://localhost/csp/report.php') do
   its(:stderr) { should match /#{conn}/ }
-end
-describe command('curl -kv https://localhost/csp/report.php'), :if => os[:family] == 'ubuntu' && os[:release] == '18.04' do
-  its(:stderr) { should match /#{conn}/ }
+  its(:stderr) { should match os[:family] }
 end
